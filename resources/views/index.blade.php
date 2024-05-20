8c0676ebf84a2d9f42f0ccc84a52522e0b20b9a3
Kacper Walenga <walenga.kacper@gmail.com>
2024-05-20 12:58:20 +0200

    success & error message

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,17 @@
-@auth()
-zalogowany jako {{ Auth::user()->email }}
-<form action="{{ route('logout') }}" method="post">
-    @csrf
-    <button type="submit">logout</button>
-</form>
+@include('shared.return-message')
+
+@auth
+    zalogowany jako {{ Auth::user()->email }}
+    <form action="{{ route('logout') }}" method="post">
+        @csrf
+        <button type="submit">logout</button>
+    </form>
 @endauth
 
 @guest
-guest
-<br>
-<a href="{{ route('login') }}">login</a>
-<br>
-<a href="{{ route('register') }}">register</a>
+    guest
+    <br>
+    <a href="{{ route('login') }}">login</a>
+    <br>
+    <a href="{{ route('register') }}">register</a>
 @endguest
