c2b3f4676541cfe876927a4e20491ae103b97319
Adam Kasprzyk <adam.kasprzyk@studenci.collegiumwitelona.pl>
2024-05-29 15:50:00 +0200

    Sprawozdanie, small changes in login and guest index

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,7 +26,8 @@ Login
             @enderror
         </div>
         <div>
-            <input type="submit" value="Login" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <input type="submit" value="Login" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">            
         </div>
     </form>
+    
 @endsection
--- resources/views/shared/login-layout.blade.php
+++ resources/views/shared/login-layout.blade.php
@@ -8,13 +8,17 @@
 </head>
 <body class="bg-blue-100 flex justify-center items-start sm:pt-3.5 h-screen">
     <div class="bg-white p-8 rounded w-full min-h-screen sm:min-h-0 max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl md:shadow-md relative">
+        <a href="{{ route('lang.switch', 'en') }}" class="absolute top-2 left-6 text-gray-800 text-sm font-medium underline">PL</a>
+        <a href="{{ route('lang.switch', 'en') }}" class="absolute top-2 left-16 text-gray-800 text-sm font-medium underline">EN</a>
         
         
         
         @yield('content')
 
 
-
+        <a href="{{ route('dashboard') }}" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-5">
+            Back 
+        </a>
     </div>
 </body>
 </html>
