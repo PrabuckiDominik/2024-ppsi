28e924af1eb57fa3f5c3baedb72c6453220eb887
Kacper Walenga <walenga.kacper@gmail.com>
2024-06-04 00:23:44 +0200

    Names instead of usernames in leaves

--- app/Http/Controllers/LeaveController.php
+++ app/Http/Controllers/LeaveController.php
@@ -2,15 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
 use App\Models\Leave;
 use App\Http\Requests\CreateLeaveRequest;
+use App\Models\User;
 
 class LeaveController extends Controller
 {
     public function index(){   
         $leaves = Leave::with('user')->get();
-        return view('leaves', compact('leaves'));
+        $employees = Employee::select('user_id', 'firstname', 'lastname')->get();
+        $usersNames = [];
+        foreach ($employees as $key => $value) {
+            $usersNames[$value->user_id] = [
+                'firstname' => $value->firstname,
+                'lastname' => $value->lastname
+            ]; 
+        }
+        return view('leaves', compact('leaves', 'usersNames'));
     }
     public function store(CreateLeaveRequest $request){
         $validated = $request->validated();
--- resources/views/leaves.blade.php
+++ resources/views/leaves.blade.php
@@ -39,7 +39,7 @@
                 <tbody>
                     @foreach ($leaves as $leave)
                     <tr>
-                        <td class="border border-gray-300">{{ $leave->user ? $leave->user->name : '---' }}</td>
+                        <td class="border border-gray-300">{{ $usersNames[$leave->user->id]['firstname'] }} {{ $usersNames[$leave->user->id]['lastname'] }}</td>
                         <td class="border border-gray-300">{{ $leave->start_date }}</td>
                         <td class="border border-gray-300">{{ $leave->end_date }}</td>
                         <td class="border border-gray-300 flex justify-center items-center">
